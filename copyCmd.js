52545ee0eaaee7bb63ae1c9d827a8afc4f6e1dec
Kishore Ithadi <ithadikishorebabu@gmail.com>
2021-07-10 18:41:25 +0530

    Delete copyCmd.js

--- copyCmd.js
+++
@@ -1,6 +0,0 @@
-// used as postinstall script
-const fs = require('fs');
-fs.copyFile('node_modules/ichatbot/ichatbotConfig.js', 'src/ichatbotConfig.js', (err) => {
-    if (err) throw err;
-    console.log('unable to copy ichatbotConfig.js');
-});
