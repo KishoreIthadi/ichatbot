a741d1dcff32f6ef00c512f7017f65f2c4ab5b86
Kishore Ithadi <ithadikishorebabu@gmail.com>
2021-07-13 20:39:42 +0530

    updated package name

--- copycmd.js
+++ copycmd.js
@@ -2,16 +2,16 @@
 const fs = require('fs');
 
 var path = require('path');
-var appDir = path.dirname(require.main.filename).replace("node_modules", "").replace("ichatbot", "");
+var appDir = path.dirname(require.main.filename).replace("node_modules", "").replace("innovative-chatbot", "");
 
 if (fs.existsSync(appDir + '/src')) {
-    fs.copyFile('README.md', appDir + '/src/README.md', (err) => {
+    fs.copyFile('ichatbotconfig.js', appDir + '/src/ichatbotconfig.js', (err) => {
         if (err)
             console.log('unable to copy ichatbotconfig.js');
     });
 }
 else {
-    fs.copyFile('README.md', '../../README.md', (err) => {
+    fs.copyFile('ichatbotconfig.js', appDir + '/ichatbotconfig.js', (err) => {
         if (err)
             console.log('unable to copy ichatbotconfig.js');
     });
